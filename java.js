e28fbb86fabdc80b2ed26c8568b550f37a79eb2f
henrique <henrique.html87@gmail.com>
2024-06-09 20:46:16 -0300

    teste com a animação de Sprits
    altura do personagem em relação ao solo.

--- java.js
+++ java.js
@@ -4,71 +4,103 @@ const ctx = canvas.getContext('2d'); // define o canvas como uma lousa 2D
 
 // dimensionamento da tela  
  window.addEventListener ( 'resize', function(){
-     
+
   
    
  })
 
-
-    
+// tratamento da tela 
     var larguraTela = window.innerWidth;  // largura da tela 
     var alturaTela = window.innerHeight; // altura da tela   
     canvas.height = alturaTela -100;
     canvas.width = larguraTela -90;  // define o tamanho do canvas baseado na tela  - 90 
 
     
-    larguraDoCanvas = canvas.clientWidth  // pega o tamanho do canvas 
-   alturaDoCanvas = canvas.clientHeight
-
-
+    larguraDoCanvas = canvas.clientWidth ; // pega o tamanho do canvas 
+   alturaDoCanvas = canvas.clientHeight;
 /////////////////////////////////////////////////////////
+
+
 
 // variaveis do player 
 let playerimage = new Image();   // objeto imagem player 
-playerimage.src="img/shadow/correndo.png"
-var poschao = alturaDoCanvas -57 ;  // altura do canvas menos o tamanho do personagem 
+playerimage.src="img/shadow/correndo.png";
+var poschao = alturaDoCanvas -61 ;  // altura do canvas menos o tamanho do personagem 
 var playerPosX = 0;  // posição do player   X
 var playerPosY = poschao; // posição do player   Y
-var playerTamanhoW =699;
+var playerTamanhoW =700;
 var playerTamanhoH = 68;
-var pulo = 30;
+var quantidadeDeSprits = 9;
+var pulo = 10;
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // area de recorte  
-var inicioDoRecorteX = 0;
+
+var indiceDoSprite = 0;
+var CorreSprit = 0;
 var inicioDoRecorteY = 0;
 var LarguraRecorte= 70 ;
 var AlturaRecorte= 70;
+
+var larguraDoSprite =  playerTamanhoW/quantidadeDeSprits;
+
+indiceDoSprite  = CorreSprit*larguraDoSprite;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-let anima;
 
-document.addEventListener('keydown',(event) =>   {
 
-if(event.keyCode===39)
-{
-    inicioDoRecorteX+=20
-    alert(inicioDoRecorteX);
+
+
+function animate (){
+     playerimage.onload =  ()=>{ 
+
  
-}
+      
+       
 
-}
-);
 
-function animate ()
+   let anima = setInterval( ()=>{ 
+  
+    indiceDoSprite  = CorreSprit*larguraDoSprite;
+    CorreSprit++; if(CorreSprit>8){CorreSprit= 0;}  
 
-{ playerimage.onload =  ()=>{ 
-   // img X
-    
-    ctx.drawImage (playerimage,inicioDoRecorteX,inicioDoRecorteY,LarguraRecorte,AlturaRecorte,playerPosX,playerPosY,70,70) 
-    alert(inicioDoRecorteX);
+    ctx.clearRect(playerPosX,playerPosY,70,70);
 
-anima =  requestAnimationFrame(animate) 
+   
+    ctx.drawImage (playerimage,indiceDoSprite,inicioDoRecorteY,LarguraRecorte,AlturaRecorte,playerPosX,playerPosY,70,70) 
+},100)
 
-} 
+
+
+
+    }
+
+    }
+  
+
+
+
+
   
 
     
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 animate();
