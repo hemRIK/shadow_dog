8b23830500da83ea18af4fbda6ca607fd1b3d5b7
henrique <henrique.html87@gmail.com>
2024-06-11 00:07:04 -0300

    animação 1
    movimento de andar , animação somente ao andar, movimento animar na próxima

--- java.js
+++ java.js
@@ -28,10 +28,11 @@ playerimage.src="img/shadow/correndo.png";
 var poschao = alturaDoCanvas -61 ;  // altura do canvas menos o tamanho do personagem 
 var playerPosX = 0;  // posição do player   X
 var playerPosY = poschao; // posição do player   Y
-var playerTamanhoW =700;
+var playerTamanhoW =playerimage.width;
 var playerTamanhoH = 68;
 var quantidadeDeSprits = 9;
 var pulo = 10;
+var velocidade =10;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,45 +42,72 @@ var pulo = 10;
 var indiceDoSprite = 0;
 var CorreSprit = 0;
 var inicioDoRecorteY = 0;
-var LarguraRecorte= 70 ;
+var LarguraRecorte= 77 ;
 var AlturaRecorte= 70;
 
 var larguraDoSprite =  playerTamanhoW/quantidadeDeSprits;
 
-indiceDoSprite  = CorreSprit*larguraDoSprite;
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+function limpa ()
+{
+    ctx.clearRect(playerPosX,playerPosY,larguraDoSprite,playerTamanhoH);
+
+
+}
 
 
 
 function animate (){
      playerimage.onload =  ()=>{ 
 
+        ctx.drawImage (playerimage,indiceDoSprite,inicioDoRecorteY,LarguraRecorte,AlturaRecorte,playerPosX,playerPosY,larguraDoSprite,playerTamanhoH) 
  
+    }
+
+
+
+    let anima = setInterval( ()=>{ 
+  
+        indiceDoSprite  = CorreSprit*larguraDoSprite;
+        
+         if(CorreSprit>8) {CorreSprit= 0;}  
       
-       
+      limpa();
+         ctx.drawImage (playerimage,indiceDoSprite,inicioDoRecorteY,LarguraRecorte,AlturaRecorte,playerPosX,playerPosY,larguraDoSprite,playerTamanhoH) 
+
+     },0.500)
+
+    }
 
 
-   let anima = setInterval( ()=>{ 
-  
-    indiceDoSprite  = CorreSprit*larguraDoSprite;
-    CorreSprit++; if(CorreSprit>8){CorreSprit= 0;}  
+    
+  window.addEventListener('keydown',(event)=>
+  {
 
-    ctx.clearRect(playerPosX,playerPosY,70,70);
 
+if(event.keyCode===39) 
+    {
+        
+       limpa();
+        playerPosX+= velocidade
+        CorreSprit++;
    
-    ctx.drawImage (playerimage,indiceDoSprite,inicioDoRecorteY,LarguraRecorte,AlturaRecorte,playerPosX,playerPosY,70,70) 
-},100)
+    }
+    else if(event.keyCode===37)
+    { 
+        limpa();
+        playerPosX-= velocidade
 
-
-
-
+ 
     }
 
-    }
-  
+
+
+  })
 
 
 
